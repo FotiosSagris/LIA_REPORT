1692495e2f96742b97b9bb241de48df8e4a55bef
FotiosSagris <2578751@edu.vaniercollege.qc.ca>
2025-12-11 17:05:12 -0500

    Update IR_remote_control.ino

--- IR_remote_control.ino
+++ IR_remote_control.ino
@@ -14,13 +14,13 @@ void setup() {
   pinMode(3, OUTPUT); // Standby
   pinMode(6, OUTPUT); // PWMB speed motor B
   pinMode(8, OUTPUT); // BIN_1 direction motor B
-Serial.begin(9600);
+Serial.begin(9600); // start the serial monitor
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Initialize the receiver
 }
 
 void loop() {
-  if (IrReceiver.decode()) {
-Forward(x);
+  if (IrReceiver.decode()) { // check if an IR signal is received
+Forward(x); 
 Backward(x);
 Right(x);
 Left(x);
@@ -28,14 +28,14 @@ Stop();
 increaseSpeed();
 decreaseSpeed();
 
- IrReceiver.resume();
+ IrReceiver.resume(); // prepare to receive the next signal
   }
         Serial.print("Current speed (bit value): ");
-    Serial.println(x);
+    Serial.println(x); // print the speed of the car
 }
 
 void Forward(int x) {
-  if ( IrReceiver.decodedIRData.decodedRawData == 0xB946FF00){
+  if ( IrReceiver.decodedIRData.decodedRawData == 0xB946FF00){ // Hex code for the Arrow pointing upwards on the IR remote
 analogWrite(5,x); // Motor A speed = x
 digitalWrite(7,1); // Motor A direction forward
 analogWrite(6,x);// Motor B speed = x
@@ -45,7 +45,7 @@ digitalWrite(3,1); // Enable motor driver (STBY)
 }
 
 void Backward ( int x){
-  if ( IrReceiver.decodedIRData.decodedRawData == 0xEA15FF00){
+  if ( IrReceiver.decodedIRData.decodedRawData == 0xEA15FF00){ // Hex code for the Arrow pointing downwards on the IR remote
 analogWrite(5,x); // Motor A speed = x
 digitalWrite(7,0); // Motor A direction forward
 analogWrite(6,x); // Motor B speed = x
@@ -54,7 +54,7 @@ digitalWrite(3,1); // Enable motor driver (STBY)
  }
 }
 void Left ( int x){
-     if ( IrReceiver.decodedIRData.decodedRawData == 0xBB44FF00){
+     if ( IrReceiver.decodedIRData.decodedRawData == 0xBB44FF00){ // Hex code for the Arrow pointing left on the IR remote
 analogWrite(5,x); // Motor A speed = x
 digitalWrite(7,1); // Motor A direction backward
 analogWrite(6,x); // Motor B speed = x
@@ -63,7 +63,7 @@ digitalWrite(3,1); // Enable motor driver (STBY)
 }
 }
 void Right ( int x){
-       if ( IrReceiver.decodedIRData.decodedRawData == 0xBC43FF00){
+       if ( IrReceiver.decodedIRData.decodedRawData == 0xBC43FF00){ // Hex code for the Arrow pointing right on the IR remote
 analogWrite(5,x); // Motor A speed = x
 digitalWrite(7,0); // Motor A direction backward
 analogWrite(6,x); // Motor B speed = x
@@ -104,7 +104,7 @@ digitalWrite(8,0); // Motor B direction backward
 digitalWrite(3,1);// Enable motor driver (STBY)
 }
 void Stop (){
-    if ( IrReceiver.decodedIRData.decodedRawData ==0xBF40FF00 ){
+    if ( IrReceiver.decodedIRData.decodedRawData ==0xBF40FF00 ){ // Hex code for the Ok button on the IR remote
 analogWrite(5,0); // Motor A speed = 0
 digitalWrite(7,1);// Direction doesn't matter when speed = 0
 analogWrite(6,0); // Motor B speed = 0
@@ -114,20 +114,20 @@ digitalWrite(3,1);
 }
 
 void increaseSpeed() {
-  if (IrReceiver.decodedIRData.decodedRawData == 0xE916FF00) {  // Speed+ button
-    x += 10;
+  if (IrReceiver.decodedIRData.decodedRawData == 0xE916FF00) {  // Speed+ button which is button number 1 on the remote
+    x += 10; // increase speed value by 10 everytime I press the button
     if (x > 255){
-      x=255;
+      x=255; // make 255 the max speed that the car can go to
     }
 
     }
   }
 
 void decreaseSpeed(){  
-  if (IrReceiver.decodedIRData.decodedRawData == 0xE619FF00) {  // Speed+ button
-    x -= 10;
+  if (IrReceiver.decodedIRData.decodedRawData == 0xE619FF00) {  // Speed - button which is button number 2 on the remote
+    x -= 10; // decrease the speed value by 10 everytime I press button number 2
     if (x > 255){
-      x=255;
+      x=255; // make 255 the max speed the car can go to
     }
   }
 }
