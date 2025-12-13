946b1274eb9875206c2c190858dcb25aee4dcbf2
FotiosSagris <2578751@edu.vaniercollege.qc.ca>
2025-12-13 12:44:29 -0500

    Update Line_Follower.ino

--- Line_Follower.ino
+++ Line_Follower.ino
@@ -1,3 +1,7 @@
+/* This program makes our robotic car follow the black tape on the floor. It uses the 3 sensors under the car to detect the black tape.
+Based on which sensor detects the line it goes forward,turns left, turns right or stops.*/
+
+
 void setup() {
   pinMode(5, OUTPUT); // PWMA speed motor A
   pinMode(7, OUTPUT); // AIN_1 Direction motor A
@@ -49,23 +53,23 @@ digitalWrite(3,1);} // Enable motor driver (STBY)
 
 
 void LineFollower() {
-  float M = analogRead(A1);
-  float L = analogRead(A2);
-  float R = analogRead(A0);
+  float M = analogRead(A1); // read middle sensor
+  float L = analogRead(A2); // read left sensor
+  float R = analogRead(A0); // read right sensor
 
-  if (M > 500 && M < 850) {
+  if (M > 500 && M < 850) { // if middle sensor detects the black line, go forward
     Forward(75);
   }
-  else if (L > 500 && L < 850) {
+  else if (L > 500 && L < 850) { // if left sensor detects the black line, go left
     Left(75);
   }
-  else if (R > 500 && R < 850) {
+  else if (R > 500 && R < 850) { // if right sensor detects the line, go right
     Right(75);
   }
-  else if (R < 400 && L < 400 && M < 400) {
+  else if (R < 400 && L < 400 && M < 400) { // if all the sensors lose the line, rotate left to search for it
     Left(75); // spin to find line
   }
-  else {
+  else { // if none of these conditions are met, stop the car
     Stop(); // all sensors off → stop
   }
 }
